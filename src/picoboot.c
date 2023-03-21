83578a6a2c6758f897d3156a8aea001c6143b160
alimao10 <51723848+alimao10@users.noreply.github.com>
2023-03-21 00:48:14 -0300

    Update picoboot.c

--- src/picoboot.c
+++ src/picoboot.c
@@ -13,7 +13,7 @@
 #include "picoboot.pio.h"
 #include "ipl.h"
 
-const uint PIN_LED = 25;                // Status LED
+const uint PIN_LED = 16;                // Status LED
 const uint PIN_DATA_BASE = 6;           // Base pin used for output, 4 consecutive pins are used 
 const uint PIN_CS = 4;                 // U10 chip select
 const uint PIN_CLK = 5;                // EXI bus clock line
